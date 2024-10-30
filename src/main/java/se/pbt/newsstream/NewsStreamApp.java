c61733c5c3a92d959dc4721b203e34be0bf71db7
Petter B <petter.bergstrom@protonmail.com>
2024-10-31 00:37:16 +0100

    Initial commit

---
+++ src/main/java/se/pbt/newsstream/NewsStreamApp.java
@@ -0,0 +1,13 @@
+package se.pbt.newsstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsStreamApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsStreamApp.class, args);
+	}
+
+}
---
+++ src/test/java/se/pbt/newsstream/NewsStreamAppTests.java
@@ -0,0 +1,13 @@
+package se.pbt.newsstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsStreamAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
