69c8da5415e4749b7d8861d9f1aa43e4aac037ac
Petter B <petter.bergstrom@protonmail.com>
2024-10-31 16:14:29 +0100

    Add api key to application.properties

--- src/main/java/se/pbt/newsstream/config/WebClientConfig.java
+++ src/main/java/se/pbt/newsstream/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package se.pbt.newsstream.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,13 +8,16 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${news.api.key}")
+    private String apiKey;
+
     private static final String BASE_URL = "https://newsapi.org/v2";
 
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
                 .baseUrl(BASE_URL)
-                .defaultHeader("Authorization", "Bearer YOUR_API_KEY") // Byt ut med din API-nyckel
+                .defaultHeader("Authorization", apiKey)
                 .build();
     }
 }
