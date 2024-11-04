bf8e114d1d5514f182a2a2309f9a9ddd61163cf7
Petter B <petter.bergstrom@protonmail.com>
2024-11-04 15:35:00 +0100

    Remove post construct method for test purpose

--- src/main/java/se/pbt/newsstream/NewsStreamApp.java
+++ src/main/java/se/pbt/newsstream/NewsStreamApp.java
@@ -1,24 +1,11 @@
 package se.pbt.newsstream;
 
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import se.pbt.newsstream.service.NewsService;
 
 @SpringBootApplication
 public class NewsStreamApp {
-	private final NewsService newsService;
-
-	public NewsStreamApp(NewsService newsService) {
-		this.newsService = newsService;
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(NewsStreamApp.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		newsService.testFetchNews();
 	}
 }
