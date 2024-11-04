5b9aed94f71510786bd5f8e3c25bbcef7a3d9950
Petter B <petter.bergstrom@protonmail.com>
2024-11-04 15:42:31 +0100

    Add functionality to controller layer to fetch news by category and topic

--- src/main/java/se/pbt/newsstream/controller/NewsController.java
+++ src/main/java/se/pbt/newsstream/controller/NewsController.java
@@ -8,17 +8,49 @@ import se.pbt.newsstream.service.NewsService;
 
 import java.util.List;
 
+/**
+ * Controller for handling HTTP requests from the user related to news articles.
+ * This class forwards the requests to the News API through the {@link NewsService} to
+ * fetch and optionally save the articles as {@link NewsArticle}.
+ */
 @RestController
 public class NewsController {
-
     private final NewsService newsService;
 
     public NewsController(NewsService newsService) {
         this.newsService = newsService;
     }
 
+
+    /**
+     * Fetches news articles based on a given topic.
+     */
     @GetMapping("/api/news")
-    public List<NewsArticle> getNews(@RequestParam String topic) {
-        return newsService.fetchNews(topic);
+    public List<NewsArticle> getNewsOnTopicWithLimit(
+            @RequestParam String topic,
+            @RequestParam(required = false, defaultValue = "10") int limit,
+            @RequestParam(required = false, defaultValue = "false") boolean storeResults) {
+
+        return newsService.fetchNews(topic, storeResults, limit);
+    }
+
+    /**
+     * Fetches news articles based on a given category.
+     */
+    @GetMapping("/api/news/category")
+    public List<NewsArticle> getNewsByCategoryWithLimit(
+            @RequestParam String category,
+            @RequestParam(required = false, defaultValue = "10") int limit,
+            @RequestParam(required = false, defaultValue = "false") boolean storeResults) {
+
+        return newsService.fetchNewsByCategory(category, storeResults, limit);
+    }
+
+    /**
+     * Fetches the latest news article based on a given topic.
+     */
+    @GetMapping("/news/latest")
+    public NewsArticle fetchLatestNewsByTopic(@RequestParam String topic) {
+        return newsService.getLatestNewsByTopic(topic);
     }
 }
