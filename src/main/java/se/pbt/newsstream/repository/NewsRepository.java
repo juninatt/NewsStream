784cc92cf0dc49004bdb586f13f48ef412fc0edc
Petter B <petter.bergstrom@protonmail.com>
2024-12-06 11:34:21 +0100

    Add annotations to repositories

--- src/main/java/se/pbt/newsstream/repository/NewsRepository.java
+++ src/main/java/se/pbt/newsstream/repository/NewsRepository.java
@@ -1,6 +1,7 @@
 package se.pbt.newsstream.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.pbt.newsstream.model.NewsArticle;
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -8,6 +9,7 @@ import java.util.Optional;
 /**
  * Repository for managing {@link NewsArticle} entities in the database.
  */
+@Repository
 public interface NewsRepository extends JpaRepository<NewsArticle, Long> {
 
     /**
--- src/main/java/se/pbt/newsstream/repository/SubscriberRepository.java
+++ src/main/java/se/pbt/newsstream/repository/SubscriberRepository.java
@@ -1,10 +1,12 @@
 package se.pbt.newsstream.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import se.pbt.newsstream.model.Subscriber;
 
 /**
  * Repository for managing {@link Subscriber} entities in the database.
  */
+@Repository
 public interface SubscriberRepository extends JpaRepository<Subscriber, Long> {
 }
